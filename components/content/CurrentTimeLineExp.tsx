1771f83c5f1a6728b5631351c79be2c1228597d2
Thang Bui Quang <89576896+thangbuiq@users.noreply.github.com>
2024-05-05 04:29:43 +0000

    feat: add bullet list

--- components/content/CurrentTimeLineExp.tsx
+++ components/content/CurrentTimeLineExp.tsx
@@ -1,4 +1,4 @@
-'use client';
+// 'use client';
 
 import { Timeline, TimelineEvent } from './TimeLineExp';
 
@@ -8,20 +8,29 @@ const CurrentTimeLineExp = () => {
 
       <TimelineEvent active>
 
-        <TimelineEvent.Title><a href='https://www.ia.com.mx' target='_blank' rel='noopener noreferrer' className='hover:text-white hover:underline transition-all ease'>KMS Technology</a> | mar. 2024 - Currently</TimelineEvent.Title>
+        <TimelineEvent.Title><a href='https://kms-technology.com/' target='_blank' rel='noopener noreferrer' className='hover:text-white hover:underline transition-all ease'><strong className='text-white'>KMS Technology</strong></a> | mar. 2024 - Currently</TimelineEvent.Title>
 
         <TimelineEvent.Description>
-          AI Engineer, working on the deployment of AI applications using Kubernetes, ArgoCD, Helm and GitHub Actions, also working on the development of NLP models using Python, TensorFlow, FastAPI, and LLM agent with LlamaIndex.
+          <ul>
+            <li><span>&#8226;</span><strong className='text-white'> AI Engineer</strong></li>
+            <li><span>&#8226;</span> Deployment of AI applications using Kubernetes, ArgoCD, Helm and GitHub Actions</li>
+            <li><span>&#8226;</span> Development of NLP models using Python, TensorFlow, FastAPI, and LLM agent with LlamaIndex</li>
+          </ul>
         </TimelineEvent.Description>
 
       </TimelineEvent>
 
       <TimelineEvent last>
 
-        <TimelineEvent.Title>Bosch Global Software Technologies | sep. 2023 - mar. 2024</TimelineEvent.Title>
+        <TimelineEvent.Title><a href='https://kms-technology.com/' target='_blank' rel='noopener noreferrer' className='hover:text-white hover:underline transition-all ease'><strong className='text-white'>Bosch Global Software Technologies</strong></a> | sep. 2023 - mar. 2024</TimelineEvent.Title>
 
         <TimelineEvent.Description>
-          DevOps Engineer, working on the deployment of applications using Docker, Jenkins, GitHub Actions, Terraform. Write scripts in Python, Bash, and Groovy. Also, working on the development of a CI/CD pipeline with store versioning and rollback using JFrog Artifactory.
+          <ul>
+            <li><span>&#8226;</span><strong className='text-white'> DevOps Engineer</strong></li>
+            <li><span>&#8226;</span> Deployment of applications using Docker, Jenkins, GitHub Actions, Terraform</li>
+            <li><span>&#8226;</span> Implement CI/CD pipelines with store versioning and rollback using JFrog Artifactory</li>
+            <li><span>&#8226;</span> Write scripts in Python, Bash, and Groovy</li>
+          </ul>
         </TimelineEvent.Description>
 
       </TimelineEvent>
